d22392c8d2df272bb97e7265b14c2be3c4fd59ff
Hiroshi Yoshida <hrysd22@gmail.com>
2020-01-13 02:16:50 +0900

    Sample

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+type Spreadsheet = GoogleAppsScript.Spreadsheet.Spreadsheet;
+type Sheet = GoogleAppsScript.Spreadsheet.Sheet;
+
+function main() {
+    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+
+    const sheet = getOrInsertSheet(spreadsheet, '時間');
+    
+    sheet.appendRow([(new Date).toUTCString()]);
+}
+
+function getOrInsertSheet(spreadsheet: Spreadsheet, sheetName: string): Sheet {
+    const sheet = spreadsheet.getSheetByName(sheetName);
+
+    if (sheet) {
+        return sheet;
+    } else {
+        return spreadsheet.insertSheet(sheetName);
+    }
+}
