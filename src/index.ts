1a07f288f86ee2bca56ed81e6c46942bb05b58d6
Hiroshi Yoshida <hrysd22@gmail.com>
2020-01-17 01:11:44 +0900

    Append temperature and humidity

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,41 @@ type Spreadsheet = GoogleAppsScript.Spreadsheet.Spreadsheet;
 type Sheet = GoogleAppsScript.Spreadsheet.Sheet;
 
 function main() {
-    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
 
-    const sheet = getOrInsertSheet(spreadsheet, '時間');
+  const sheet = getOrInsertSheet(spreadsheet, '時間');
 
-    sheet.appendRow([
-        (new Date).toUTCString(),
-        process.env.SECRET
-    ]);
+  const device = fetchDevice(process.env.TOKEN);
+
+  const { te, hu } = device.newest_events
+
+  sheet.appendRow([
+    (new Date).toUTCString(),
+    te.val,
+    hu.val
+  ]);
 }
 
 function getOrInsertSheet(spreadsheet: Spreadsheet, sheetName: string): Sheet {
-    const sheet = spreadsheet.getSheetByName(sheetName);
+  const sheet = spreadsheet.getSheetByName(sheetName);
 
-    if (sheet) {
-        return sheet;
-    } else {
-        return spreadsheet.insertSheet(sheetName);
-    }
+  if (sheet) {
+    return sheet;
+  } else {
+    return spreadsheet.insertSheet(sheetName);
+  }
+}
+
+const API_ENDPOINT = 'https://api.nature.global/1/devices';
+
+function fetchDevice(token) {
+   const response = UrlFetchApp.fetch(API_ENDPOINT, {
+     headers: {
+       'Content-Type': 'application/json',
+       'Authorization': `Bearer ${token}`
+     }
+   });
+
+   // NOTE: I have only one device ;)
+   return response[0];
 }
